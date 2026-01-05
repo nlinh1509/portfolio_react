a7a212842bad23cc48832a751857e37ec05f1bb4
Lê Võ Nhật Linh <levonhatlinh.work@gmail.com>
2026-01-05 16:55:18 +0700

    reset

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -10,16 +10,6 @@ export const assets = {
 export const projectsData = [
   {
     id: 1,
-    title: "EcoTrack - Waste Sorting App",
-    category: "Mobile",
-    description:
-      "EcoTrack is a cross-platform mobile app that leverages AI to assist users in classifying waste accurately. Through gamification and eco-friendly incentives.",
-    image: iphone,
-    // technologies: ["React", "AI", "CSS"],
-  },
-
-  {
-    id: 2,
     title: "Food delivery",
     category: "Web",
     description:
@@ -27,12 +17,22 @@ export const projectsData = [
     image: laptop,
     // technologies: ["React", "AI", "CSS"],
   },
-    {
-    id: 3,
+  {
+    id: 2,
     title: "Portfolio Website",
     category: "Web",
     description: "My personal portfolio showcasing my projects and skills.",
     image: laptop,
     // technologies: ["React", "Framer Motion"],
+  },
+
+  {
+    id: 3,
+    title: "EcoTrack - Waste Sorting App",
+    category: "Mobile",
+    description:
+      "EcoTrack is a cross-platform mobile app that leverages AI to assist users in classifying waste accurately. Through gamification and eco-friendly incentives.",
+    image: iphone,
+    // technologies: ["React", "AI", "CSS"],
   },
 ];
--- frontend/src/components/Project/Project.jsx
+++ frontend/src/components/Project/Project.jsx
@@ -3,42 +3,14 @@ import "./Project.css";
 import { projectsData } from "../../assets/assets";
 
 const Project = () => {
+  const webProjects = projectsData.filter((project)=> project.category === "Web");
+  const mobileProjects = projectsData.filter((project) => project.category === "Mobile");
+  
   return (
-    <div className="project">
-      <div className="header-section">
-        <h1>Project</h1>
-        <p>Here are some of my favorite projects.</p>
-      </div>
 
-      <div className="project-list">
-        {projectsData.map((project, index) => {
-          // Logic để xác định kiểu hiển thị dựa trên Category
-          let cardClass = "project-card";
-          
-          if (project.category === "Web") {
-            cardClass += " wide"; // Thêm class wide cho Web
-          } else if (project.category === "Mobile") {
-            cardClass += " tall"; // Thêm class tall cho Mobile
-          }
 
-          return (
-            <div key={index} className={cardClass}>
-              <div className="card-img">
-                <img src={project.image} alt={project.title} />
-              </div>
-              
-              <div className="card-info">
-                <span className="category-tag">{project.category}</span>
-                <h3>{project.title}</h3>
-                <p>{project.description}</p>
-                {/* Nút xem chi tiết giống trong hình */}
-                <button className="view-btn">View Case Study</button>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </div>
+
+    
   );
 };
 
